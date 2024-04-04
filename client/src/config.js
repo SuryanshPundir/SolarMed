bb6701d390dd4f86ceac93d3ce5dbcdf76804134
ImRishabhRawat <rishabhrawat757@gmail.com>
2024-04-04 14:55:33 +0530

    update config

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:8000/api/v1";
-// export const BASE_URL = "https://solarmed-v2.onrender.com/api/v1";
+// export const BASE_URL = "http://localhost:8000/api/v1";
+export const BASE_URL = "https://solarmed-v2.onrender.com/api/v1";
 export const token = localStorage.getItem("token");
